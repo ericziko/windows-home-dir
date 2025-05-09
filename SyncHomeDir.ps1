cff63978f5e9968cd1252a47ef58f36171c5fdb9
Eric Ziko <eric.ziko@gmail.com>
2025-05-08 22:42:33 -0700

    Added SyncHomeDir.ps1

---
+++ SyncHomeDir.ps1
@@ -0,0 +1,15 @@
+# Set current directory to the script's directory
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location -Path $scriptDir
+
+
+$homeDir = $env:USERPROFILE
+$sourceDir = "$homeDir\.vscode\extensions"
+$targetDir = "$scriptDir\.vscode\extensions"
+
+# if the target directory does not exist, create it
+if (-not (Test-Path -Path $targetDir)) {
+    New-Item -ItemType Directory -Path $targetDir
+}
+
+Robocopy.exe $sourceDir $targetDir /E /L
